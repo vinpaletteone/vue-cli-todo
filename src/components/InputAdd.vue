2f0b851de84f55865a6768fa30b3267696f763e2
“vinpaletteone”  <binsuzy95@gmail.com>
2021-07-18 22:49:22 +0900

    todo 추가 기능

--- src/components/InputAdd.vue
+++ src/components/InputAdd.vue
@@ -1,12 +1,43 @@
 <template>
     <div>
-        <input type="text" placeholder="입력하세요!">
-        <button>등록</button>
+        <input type="text" placeholder="입력하세요!" v-model="newItem" @keyup.enter="addItem">
+        <button @click="addItem">등록</button>
     </div>
 </template>
 
 <script>
 export default {
-    
+    data(){
+        return{
+            newItem: ''
+        }
+    },
+    created(){
+        const todoList = localStorage.getItem('todoList');
+
+        if(todoList == null) return;
+        this.$store.commit('todoSet', JSON.parse(todoList));
+    },
+    methods : {
+        addItem(){
+            if(this.newItem!==''){
+                let value = {
+                    item:this.newItem,
+                    date:`${new Date().getMonth()+1}/${new Date().getDate()}`,
+                    completed: false
+                }
+
+                this.todoAdd(value);
+                this.$store.commit('todoLocalSet');
+                this.clearInput();
+            }
+        },
+        clearInput(){
+            this.newItem = '';
+        },
+        todoAdd(value){
+            this.$store.commit('todoAdd',value);
+        }
+    }
 }
 </script>
