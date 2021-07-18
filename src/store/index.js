c8e98e28b83cb773a5ab733e1c04a7fa7fb45d52
“vinpaletteone”  <binsuzy95@gmail.com>
2021-07-18 22:50:42 +0900

    로컬스토리지에 todo 추가 / 삭제 / 수정

--- src/store/index.js
+++ src/store/index.js
@@ -5,8 +5,43 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    total:0,
+    todoList:[]
+  },
+  getters:{
+    getTodoList : function(state){
+      return state.todoList;
+    }
   },
   mutations: {
+    increment (state) {
+      state.total++
+    },
+    decrement (state) {
+      state.total--
+    },
+    todoAdd(state, value){
+      state.todoList.push(value);
+    },
+    todoSet(state, arr){
+      state.todoList = arr;
+    },
+    todoRemove(state, idx){
+      for(let i=0; i<=state.todoList.length; i++){
+        if(idx===i){
+            state.todoList.splice(i, 1);
+        }
+      }
+    },
+    todoLocalSet(state){
+      localStorage.setItem('todoList', JSON.stringify(state.todoList));
+    },
+    todoRemoveAll(state){
+      localStorage.removeItem('todoList');
+      state.todoList = [];
+      console.log(state.todoList);
+    }
+
   },
   actions: {
   },
