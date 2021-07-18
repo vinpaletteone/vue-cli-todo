f16b17831c3cedd21af5518abbda30ba74528fe7
“vinpaletteone”  <binsuzy95@gmail.com>
2021-07-18 22:51:30 +0900

    Title view 추가

--- src/components/vTitle.vue
+++ src/components/vTitle.vue
@@ -1,10 +1,24 @@
 <template>
-    <div>
-        Todolist !
+    <div class="title">
+        <p>Hi, {{ name }} !</p>
+        <span>{{ today }}</span>
+        <p>총 할 일 : {{ total }}</p>
     </div>
 </template>
 <script>
 export default {
-    
+    data(){
+        return{
+            name : 'Subin',
+            total : 0
+        }
+    },
+    created(){
+        const nDate = new Date();
+        const month = nDate.getMonth() + 1;
+        const date = nDate.getDate();
+
+        this.today = `${month}/${date}`;
+    }
 }
 </script>
