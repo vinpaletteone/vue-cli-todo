2038f72f6cea7641350a97838dbc7daa662cd959
“vinpaletteone”  <binsuzy95@gmail.com>
2021-07-18 22:50:55 +0900

    전체 삭제 기능 추가중

---
+++ src/components/vFilter.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        <button @click="todoRemoveAll">전체삭제</button>
+    </div>
+</template>
+
+<script>
+export default {
+    methods : {
+        todoRemoveAll(){
+            this.$store.commit('todoRemoveAll');
+            console.log(this.$store.getters.getTodoList);
+        }
+    }
+}
+</script>
